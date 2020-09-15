6770b148a788c1196ba2189674c36e6f98cdc1ef
Oohira Suguru <suguru.13579515@gmail.com>
2020-09-15 10:58:35 +0900

    Start paizaJava

---
+++ question.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+
+        System.out.println("paiza");
+    }
+}
