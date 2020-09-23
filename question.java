3d561e836ee41093ec1dbacb639eed5a8d8baa4f
Oohira Suguru <suguru.13579515@gmail.com>
2020-09-23 20:45:16 +0900

    Today finsh paiza

--- question.java
+++ question.java
@@ -74,5 +74,15 @@ public class Main {
         }else{
             System.out.println("NO");
         }
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String strr = sc.next();
+        int d = strr.indexOf(str);
+        if(d >= 0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 }
