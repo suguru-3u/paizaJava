f867f8782abc6ba718ffd1a27125a528797f2a73
Oohira Suguru <suguru.13579515@gmail.com>
2020-09-28 23:12:49 +0900

    Finish today paiza question

--- question.java
+++ question.java
@@ -112,5 +112,19 @@ public class Main {
                 System.out.println(msg[count]);
             }
         }
+
+        Scanner sc = new Scanner(System.in);
+         String string = sc.next();
+
+         String[] msg = string.split(":", 0);
+
+         for(int count = 0; count < msg.length ; count++){
+             int s = msg[count].indexOf("0");
+             if(s == 0){
+                 System.out.println(msg[count].charAt(1));
+             }else{
+                 System.out.println(msg[count]);
+             }
+         }
     }
 }
